652c458c815473c5263c670af1ea188653e06d43
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-02-04 22:15:55 +0100

    Create static page with python script

---
+++ render.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import toml
+import pystache
+from os import path
+import os
+
+PAGES_FOLDER = 'pages'
+PUBLIC_FOLDER = 'public'
+
+with open('config.toml') as f:
+    config = toml.loads(f.read())
+
+with open('template.mustache') as f:
+    template = f.read()
+
+for page in config['pages']:
+    page_file = path.join(PAGES_FOLDER, page['file'] + '.mustache')
+
+    with open(page_file) as f:
+        page_template = f.read()
+
+    nav = []
+    for nav_page in config['pages']:
+        if 'title' in nav_page:
+            nav.append({
+                'path': nav_page['path'],
+                'title': nav_page['title'],
+                'is_active': nav_page['path'] == page['path'],
+            })
+
+    if 'title' in page:
+        title = page['title'] + ' · ' + config['title']
+    else:
+        title = config['title']
+
+    context = { 'nav': nav, 'config': config, 'page': page, 'title': title }
+    context['content'] = pystache.render(page_template, context)
+
+    page_html = pystache.render(template, context)
+
+    with open(path.join(PUBLIC_FOLDER, page['file'] + '.html'), 'w') as f:
+        f.write(page_html)
