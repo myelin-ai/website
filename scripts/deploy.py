cd73d7a27509db70d01f75288ab29850f4079b2a
Ruben Schmidmeister <ruben@myelin.ch>
2019-08-23 10:14:13 +0200

    Deploy to github pages

---
+++ scripts/deploy.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import tempfile
+import subprocess
+from typing import List
+import os
+import shutil
+
+TEMP_DIR = tempfile.mkdtemp()
+TARGET_BRANCH = 'gh-pages'
+PUBLIC_DIR = 'public'
+
+
+def _run_git_command(args: List[str]):
+    subprocess.check_call(['git', '-C', TEMP_DIR, *args])
+
+
+def _get_remote_url() -> str:
+    remote_url = subprocess.check_output(['git', 'remote', 'get-url', 'origin']).decode('utf-8').strip()
+    github_token = os.environ['GITHUB_TOKEN']
+    return remote_url.replace('https://', f'https://{github_token}@')
+    
+def _clone_repository():
+    subprocess.check_call(['git', 'clone', _get_remote_url(), '--quiet', '-b', TARGET_BRANCH, TEMP_DIR])
+
+def _remove_all_files():
+    _run_git_command(['rm', '-r', '.'])
+
+def _copy_files_to_repository():
+    shutil.copytree(PUBLIC_DIR, TEMP_DIR)
+
+def _commit_all_changes():
+    commit = os.environ['GITHUB_SHA']
+    _run_git_command(['add', '-A'])
+    _run_git_command(['commit', '-m', f'Build for {commit}'])
+
+def _push():
+    _run_git_command(['push'])
+
+try:
+    print(TEMP_DIR)
+    _clone_repository()
+    _remove_all_files()
+    _copy_files_to_repository()
+    _commit_all_changes()
+    _push()
+finally:
+    shutil.rmtree(TEMP_DIR)
